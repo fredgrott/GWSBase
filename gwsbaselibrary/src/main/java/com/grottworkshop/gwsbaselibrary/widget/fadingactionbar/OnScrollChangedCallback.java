20f2184739e83b41031e00e8dce848e7d3cdf168
Fred Grott <fred.grott@gmail.com>
2014-10-09 07:37:18 -0700

    added fadingactionbar code

---
+++ gwsbaselibrary/src/main/java/com/grottworkshop/gwsbaselibrary/widget/fadingactionbar/OnScrollChangedCallback.java
@@ -0,0 +1,8 @@
+package com.grottworkshop.gwsbaselibrary.widget.fadingactionbar;
+
+/**
+ * Created by fgrott on 10/9/2014.
+ */
+public interface OnScrollChangedCallback {
+    void onScroll(int l, int t);
+}
---
+++ gwsbaselibrary/src/main/java/com/grottworkshop/gwsbaselibrary/widget/fadingactionbar/Utils.java
@@ -0,0 +1,16 @@
+package com.grottworkshop.gwsbaselibrary.widget.fadingactionbar;
+
+import android.content.Context;
+import android.view.WindowManager;
+
+/**
+ * Created by fgrott on 10/9/2014.
+ */
+public class Utils {
+    public static int getDisplayHeight(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        @SuppressWarnings("deprecation")
+        int displayHeight = wm.getDefaultDisplay().getHeight();
+        return displayHeight;
+    }
+}
