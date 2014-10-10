6d35ba642988ec212f8fd5a122b638bf728e87ec
Fred Grott <fred.grott@gmail.com>
2014-10-10 14:16:36 -0700

    added shimmer widget code

---
+++ gwsbaselibrary/src/main/java/com/grottworkshop/gwsbaselibrary/widget/shimmer/ShimmerViewBase.java
@@ -0,0 +1,18 @@
+package com.grottworkshop.gwsbaselibrary.widget.shimmer;
+
+/**
+ * Created by fgrott on 10/10/2014.
+ */
+public interface ShimmerViewBase {
+
+    public float getGradientX();
+    public void setGradientX(float gradientX);
+    public boolean isShimmering();
+    public void setShimmering(boolean isShimmering);
+    public boolean isSetUp();
+    public void setAnimationSetupCallback(ShimmerViewHelper.AnimationSetupCallback callback);
+    public int getPrimaryColor();
+    public void setPrimaryColor(int primaryColor);
+    public int getReflectionColor();
+    public void setReflectionColor(int reflectionColor);
+}
